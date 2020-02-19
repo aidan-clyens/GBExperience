be86152ba008aba4fcc289af3e52c9db6aae4ba8
Aidan Clyens <aidanc@rogers.com>
2020-02-19 00:36:29 -0500

    CPU registers can be be read and written to as 8 bits or together as 16 bits

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -4,6 +4,8 @@
 #include <exception>
 #include <string>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 #include "../memory/memory_map.h"
 
@@ -19,7 +21,16 @@ class CPU {
         uint16_t read_register(const std::string &);
 
     private:
-        std::map<std::string, uint16_t> m_registers;
+        bool _check_register_valid(const std::string &);
+        void _write_8bit_register(const std::string &, uint8_t);
+        uint8_t _read_8bit_register(const std::string &);
+
+        std::vector<std::string> m_valid_8bit_registers;
+        std::vector<std::string> m_valid_16bit_registers;
+
+        std::map<std::string, uint8_t> m_registers;
+        uint16_t m_PC;
+        uint16_t m_SP;
 
         MemoryMap &m_memory_map;
 };
