b7f6a0c40db84d53d1b30b1cc8726ea5c7a7c539
Aidan <aidanc@rogers.com>
2020-02-21 09:55:37 -0500

    Created separate CPU class for registers

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -2,11 +2,8 @@
 
 #include <iostream>
 #include <exception>
-#include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
 
+#include "cpu_registers.h"
 #include "../memory/memory_map.h"
 
 
@@ -21,16 +18,6 @@ class CPU {
         uint16_t read_register(const std::string &);
 
     private:
-        bool _check_register_valid(const std::string &);
-        void _write_8bit_register(const std::string &, uint8_t);
-        uint8_t _read_8bit_register(const std::string &);
-
-        std::vector<std::string> m_valid_8bit_registers;
-        std::vector<std::string> m_valid_16bit_registers;
-
-        std::map<std::string, uint8_t> m_registers;
-        uint16_t m_PC;
-        uint16_t m_SP;
-
+        CPURegisters m_registers;
         MemoryMap &m_memory_map;
 };
