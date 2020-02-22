448787cf431743a63d27bef9cfa02e130556869d
Aidan <aidanc@rogers.com>
2020-02-21 22:15:34 -0500

    Add function headers to 8-Bit ALU commands and test cases for 8-Bit ALU

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -21,4 +21,30 @@ class CPU {
     private:
         CPURegisters m_registers;
         MemoryMap &m_memory_map;
+
+        /****    8-Bit ALU    ****/
+        // ADD A, n
+        // ADC A, n
+        void alu_add(const std::string &, bool);
+        void alu_add(uint8_t, bool);
+        // SUB n
+        // SBC n
+        void alu_sub(const std::string &, bool);
+        void alu_sub(uint8_t, bool);
+        // AND n
+        void alu_and(const std::string &);
+        void alu_and(uint8_t);
+        // OR n
+        void alu_or(const std::string &);
+        void alu_or(uint8_t);
+        // XOR n
+        void alu_xor(const std::string &);
+        void alu_xor(uint8_t);
+        // CP n
+        void alu_cp(const std::string &);
+        void alu_cp(uint8_t);
+        // INC n
+        void alu_inc(const std::string &);
+        // DEC n
+        void alu_dec(const std::string &);
 };
