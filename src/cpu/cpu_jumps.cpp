580e1d60118ca1d9b6aea7c207b7cda444db8ada
Aidan Clyens <aidanc@rogers.com>
2020-03-03 13:07:18 -0500

    Implement CPU Jump instructions

--- src/cpu/cpu_jumps.cpp
+++ src/cpu/cpu_jumps.cpp
@@ -2,25 +2,40 @@
 
 // JP nn
 void CPU::jump(uint16_t value) {
-    // TODO JP nn
+    this->write_register("PC", value);
 }
 
 // JP cc, nn
 void CPU::jump_conditional(uint16_t value, CPUFlag_t flag, bool set) {
-    // TODO JP cc, nn
+    bool flag_set = this->read_flag_register(flag);
+
+    if (!(flag_set ^ set)) {
+        this->write_register("PC", value);
+    }
 }
 
 // JP (HL)
 void CPU::jump_hl() {
-    // TODO JP (HL)
+    uint16_t value = this->read_register("HL");
+
+    this->write_register("PC", value);
 }
 
 // JR n
 void CPU::jump_add(uint8_t value) {
-    // TODO JR n
+    uint16_t pc = this->read_register("PC");
+
+    // TODO Implement error checking for PC overflow
+    this->write_register("PC", pc + value - 1);
 }
 
 // JR cc, n
 void CPU::jump_add_conditional(uint8_t value, CPUFlag_t flag, bool set) {
-    // TODO JR cc, n
+    uint16_t pc = this->read_register("PC");
+    bool flag_set = this->read_flag_register(flag);
+
+    // TODO Implement error checking for PC overflow
+    if (!(flag_set ^ set)) {
+        this->write_register("PC", pc + value - 1);
+    }
 }
