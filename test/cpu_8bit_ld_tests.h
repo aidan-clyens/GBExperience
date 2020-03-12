f4ed3574ea1d793bdb15ae357a5b4fd0667b80ba
Aidan <aidanc@rogers.com>
2020-03-12 11:02:54 -0400

    Add LD A, (C) and LD (C), A instructions. Test cases will fail until memory-mapped I/O is implemented

--- test/cpu_8bit_ld_tests.h
+++ test/cpu_8bit_ld_tests.h
@@ -267,3 +267,58 @@ TEST(CPU_LD, LD_NN_A) {
     cpu.write_register("BC", address);
     EXPECT_EQ(val_2, cpu.read_memory("BC"));
 }
+
+// LD A, (C)
+TEST(CPU_LD, LD_A_C) {
+    // TODO Fix when memory-mapped I/O is implemented
+    uint8_t opcode = 0xF2;
+    uint8_t val_1 = 0x20;
+    uint8_t val_2 = 0xAC;
+    uint8_t val_C = 0x14;
+    uint16_t address = 0xFF00 + val_C;
+
+    MemoryMap mem_map;
+    mem_map.init_memory_map(nullptr);
+    mem_map.write(address, val_2);
+    
+    EXPECT_EQ(val_2, mem_map.read(address));
+    
+    CPU cpu(mem_map);
+    
+    cpu.write_register("A", val_1);
+    cpu.write_register("C", val_C);
+
+    EXPECT_EQ(val_1, cpu.read_register("A"));
+    EXPECT_EQ(val_C, cpu.read_register("C"));
+
+    cpu.decode_op(opcode);
+
+    EXPECT_EQ(val_2, cpu.read_register("A"));
+}
+
+
+// LD (C), A
+TEST(CPU_LD, LD_C_A) {
+    // TODO Fix when memory-mapped I/O is implemented
+    uint8_t opcode = 0xE2;
+    uint8_t val_1 = 0x20;
+    uint8_t val_2 = 0xAC;
+    uint8_t val_C = 0x14;
+    uint16_t address = 0xFF00 + val_C;
+
+    MemoryMap mem_map;
+    mem_map.init_memory_map(nullptr);
+    mem_map.write(address, val_1);
+    CPU cpu(mem_map);
+    
+    cpu.write_register("C", val_C);
+    cpu.write_register("A", val_2);
+
+    EXPECT_EQ(val_C, cpu.read_register("C"));
+    EXPECT_EQ(val_2, cpu.read_register("A"));
+
+    cpu.decode_op(opcode);
+
+    cpu.write_register("DE", address);
+    EXPECT_EQ(val_2, cpu.read_memory("DE"));
+}
