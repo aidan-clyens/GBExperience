4e4f00d12692bd75c3aba93a49546690b982b9dd
Aidan <aidanc@rogers.com>
2020-03-24 19:34:28 -0400

    Store a list of times, not associated with an opcode

--- src/debugger/timing_analyzer.h
+++ src/debugger/timing_analyzer.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <chrono>
 #include <unordered_map>
+#include <list>
 
 
 class TimingAnalyzer {
@@ -13,13 +14,17 @@ class TimingAnalyzer {
 
         void get_start_time();
         float get_time_difference_us();
+        float get_time_difference_ns();
 
         void log_cycle_time(uint8_t, long int);
+        void log_time(long int);
         void save_cycle_times();
+        void save_times();
 
     private:
         std::chrono::steady_clock::time_point m_last_time;
         std::unordered_map<uint8_t, long int> m_operation_times;
+        std::list<long int> m_times;
 
         std::string m_file_name;
 
