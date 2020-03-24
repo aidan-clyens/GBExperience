9a529365bfaa8ed3353070271beed3470b8ace3e
Aidan Clyens <aidanc@rogers.com>
2020-03-24 19:34:28 -0400

    Store a list of times, not associated with an opcode

--- src/debugger/timing_analyzer.cpp
+++ src/debugger/timing_analyzer.cpp
@@ -20,12 +20,21 @@ float TimingAnalyzer::get_time_difference_us() {
     return std::chrono::duration_cast<std::chrono::microseconds>(end - m_last_time).count();    
 }
 
+float TimingAnalyzer::get_time_difference_ns() {
+    std::chrono::steady_clock::time_point end = get_time();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_last_time).count();    
+}
+
 std::chrono::steady_clock::time_point TimingAnalyzer::get_time() {
     return std::chrono::steady_clock::now();
 }
 
 void TimingAnalyzer::log_cycle_time(uint8_t opcode, long int dt) {
     m_operation_times.emplace(opcode, dt);
+}
+
+void TimingAnalyzer::log_time(long int dt) {
+    m_times.push_back(dt);
 }
 
 void TimingAnalyzer::save_cycle_times() {
@@ -39,6 +48,22 @@ void TimingAnalyzer::save_cycle_times() {
         long int dt = it->second;
 
         result_file << static_cast<int>(opcode) << "," << dt << "\n";
+    }
+
+    result_file.close();
+
+    std::cout << "Saved timing analysis to: " << m_file_name << std::endl;
+}
+
+void TimingAnalyzer::save_times() {
+    std::list<long int>::iterator it;
+
+    std::ofstream result_file;
+    result_file.open(m_file_name);
+
+    for (it = m_times.begin(); it != m_times.end(); it++) {
+        long int dt = *it;
+        result_file << dt << "\n";
     }
 
     result_file.close();
