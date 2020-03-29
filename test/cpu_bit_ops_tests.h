679d1f60ed2f2278419959013b568c2e49f48d68
Aidan <aidanc@rogers.com>
2020-03-29 14:09:32 -0400

    Add CPU BIT b,r instruction

--- test/cpu_bit_ops_tests.h
+++ test/cpu_bit_ops_tests.h
@@ -1,2 +1,56 @@
 #include "gtest/gtest.h"
 #include "cpu/cpu.h"
+
+
+// BIT 2, B
+TEST(CPU_BIT_OPS, BIT_B_R) {
+    uint16_t PC = 0xA000;
+    uint8_t opcode = 0x50;
+    uint8_t B = 0xAB;     // 1010 1011
+    bool is_zero = true;
+
+    MemoryMap mem_map = setup_cb_instruction(PC, opcode);
+    CPU cpu(mem_map);
+
+    cpu.write_register("PC", PC);
+    cpu.write_register("B", B);
+    EXPECT_EQ(PC, cpu.read_register("PC"));
+    EXPECT_EQ(B, cpu.read_register("B"));
+
+    opcode = cpu.fetch_op();
+    EXPECT_EQ(0xCB, opcode);
+
+    cpu.decode_op(opcode);
+
+    EXPECT_EQ(is_zero, cpu.read_flag_register(ZERO_FLAG));
+    EXPECT_EQ(false, cpu.read_flag_register(SUBTRACT_FLAG));
+    EXPECT_EQ(true, cpu.read_flag_register(HALF_CARRY_FLAG));
+}
+
+// BIT 1, (HL)
+TEST(CPU_BIT_OPS, BIT_B_HL) {
+    uint16_t PC = 0xA000;
+    uint8_t opcode = 0x4E;
+    uint16_t HL = 0xA0FF;
+    uint8_t val = 0xAB;     // 1010 1011
+    bool is_zero = false;
+
+    MemoryMap mem_map = setup_cb_instruction(PC, opcode);
+    CPU cpu(mem_map);
+
+    cpu.write_register("PC", PC);
+    cpu.write_register("HL", HL);
+    cpu.write_memory(val);
+    EXPECT_EQ(PC, cpu.read_register("PC"));
+    EXPECT_EQ(HL, cpu.read_register("HL"));
+    EXPECT_EQ(val, cpu.read_memory());
+
+    opcode = cpu.fetch_op();
+    EXPECT_EQ(0xCB, opcode);
+
+    cpu.decode_op(opcode);
+
+    EXPECT_EQ(is_zero, cpu.read_flag_register(ZERO_FLAG));
+    EXPECT_EQ(false, cpu.read_flag_register(SUBTRACT_FLAG));
+    EXPECT_EQ(true, cpu.read_flag_register(HALF_CARRY_FLAG));
+}
