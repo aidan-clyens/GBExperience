29eb03c18f1d8b571ccd0d0af9db20330fb6ae30
Aidan Clyens <aidanc@rogers.com>
2020-04-23 12:05:03 -0400

    Add enum with I/O register addresses

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -19,6 +19,27 @@ typedef enum CPUFlag {
     CARRY_FLAG = 0x10
 } CPUFlag_t;
 
+typedef enum IORegisters {
+    P1 = 0xFF00,
+    DIV = 0xFF04,
+    TIMA = 0xFF05,
+    TMA = 0xFF06,
+    TAC = 0xFF07,
+    IF = 0xFF0F,
+    LCDC = 0xFF40,
+    STAT = 0xFF41,
+    SCY = 0xFF42,
+    SCX = 0xFF43,
+    LY = 0xFF44,
+    LYC = 0xFF45,
+    DMA = 0xFF46,
+    BGP = 0xFF47,
+    OBP0 = 0xFF48,
+    OBP1 = 0xFF49,
+    WY = 0xFF4A,
+    WX = 0xFF4B,
+    IE = 0xFFFF
+} IORegisters_t;
 
 class CPU {
     public:
