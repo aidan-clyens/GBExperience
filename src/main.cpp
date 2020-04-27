4d87e34d84b7947a79a012c01e1c348590cccd49
Aidan Clyens <aidanc@rogers.com>
2020-04-27 17:22:29 -0400

    Create GameBoy class

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 
 #include "display/display.h"
-#include "file_parser/file_parser.h"
-#include "memory/memory_map.h"
-#include "memory/memory.h"
-#include "cpu/cpu.h"
+#include "gameboy.h"
 
 
 int main(int argc, char** argv) {
@@ -17,28 +14,16 @@ int main(int argc, char** argv) {
         rom_file = argv[1];
     }
 
-    FileParser file_parser(rom_size);
-    file_parser.load_rom(rom_file);
-    
-    MemoryMap memory_map;
-    memory_map.load_rom(file_parser.get_buffer_ptr());
 
-    CPU cpu(memory_map);
+    GameBoy gb;
+    gb.load_rom(rom_file, rom_size);
 
-    std::string rom_name = file_parser.get_rom_name();
-    cartridge_type_t cartridge_type = file_parser.get_cartridge_type();
-
-    Display main_display(rom_name);
-
-    std::cout << "Title: " << rom_name << std::endl;
-    std::cout << "Cartridge Type: " << cartridge_type << std::endl;
-    std::cout << "GameBoy Color: " << file_parser.is_gb_color() << std::endl;
-    std::cout << "Super GameBoy: " << file_parser.is_sgb() << std::endl << std::endl;
+    Display main_display(gb.get_rom_name());
 
     main_display.init_display();
     
     while (main_display.is_display_open()) {
-        cpu.tick();
+        gb.tick();
 
         main_display.poll_events();
         main_display.render();
