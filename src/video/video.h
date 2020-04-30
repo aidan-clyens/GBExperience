019bdced3ee454b94bd1d04d3ae092f6d0826117
Aidan <aidanc@rogers.com>
2020-04-29 21:15:36 -0400

    Get sprite size from LCDC register

--- src/video/video.h
+++ src/video/video.h
@@ -3,6 +3,12 @@
 #include "../memory/memory_map.h"
 
 
+typedef enum SpriteSize {
+    SPRITEx8,
+    SPRITEx16
+} SpriteSize_t;
+
+
 class Video {
     public:
         Video(MemoryMap &);
@@ -15,6 +21,7 @@ class Video {
         bool window_display_enabled();
         bool sprite_display_enabled();
         bool background_display_enabled();
+        SpriteSize_t get_sprite_size();
 
         uint8_t read_io_register(IORegisters_t);
         void write_io_register(IORegisters_t, uint8_t);
