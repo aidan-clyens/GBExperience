a9c65fa821e94dc2b98312e735c5d53c5be556f2
Aidan Clyens <aidanc@rogers.com>
2020-05-04 22:10:34 -0400

    Reset FrameBuffer

--- src/video/framebuffer.cpp
+++ src/video/framebuffer.cpp
@@ -23,3 +23,11 @@ void FrameBuffer::set_pixel(int x, int y, Colour_t colour) {
     int index = y * m_width + x;
     m_buffer[index] = colour;
 }
+
+void FrameBuffer::reset() {
+    for (int y = 0; y < m_height; y++) {
+        for (int x = 0; x < m_width; x++) {
+            this->set_pixel(x, y, WHITE);
+        }
+    }
+}
