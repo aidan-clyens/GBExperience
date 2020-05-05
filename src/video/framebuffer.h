a756244da306cd24190cca20cfec2066d4f8228d
Aidan <aidanc@rogers.com>
2020-05-04 22:10:34 -0400

    Reset FrameBuffer

--- src/video/framebuffer.h
+++ src/video/framebuffer.h
@@ -12,6 +12,8 @@ class FrameBuffer {
 
         Colour_t get_pixel(int, int);
         void set_pixel(int, int, Colour_t);
+
+        void reset();
     
     private:
         std::vector<Colour_t> m_buffer;
