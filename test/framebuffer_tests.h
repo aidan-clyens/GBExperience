b876f633830b43a650442cc87473be2c75a54eff
Aidan <aidanc@rogers.com>
2020-05-04 22:10:34 -0400

    Reset FrameBuffer

--- test/framebuffer_tests.h
+++ test/framebuffer_tests.h
@@ -37,3 +37,25 @@ TEST(FrameBuffer, SetPixel) {
         }
     }
 }
+
+
+TEST(FrameBuffer, ResetFrameBuffer) {
+    int width = 256;
+    int height = 256;
+    FrameBuffer buffer(width, height);
+
+    buffer.set_pixel(10, 10, BLACK);
+    buffer.set_pixel(20, 25, LIGHT_GRAY);
+    buffer.set_pixel(50, 100, LIGHT_GRAY);
+    buffer.set_pixel(100, 75, DARK_GRAY);
+    buffer.set_pixel(20, 80, DARK_GRAY);
+    buffer.set_pixel(15, 90, BLACK);
+
+    buffer.reset();
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            EXPECT_EQ(WHITE, buffer.get_pixel(x, y));
+        }
+    }
+}
