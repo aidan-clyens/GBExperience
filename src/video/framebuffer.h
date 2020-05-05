25754c9d8b2d12de30e9d03cb5a755d7d2a0f5aa
Aidan <aidanc@rogers.com>
2020-05-04 21:25:10 -0400

    Add FrameBuffer class

---
+++ src/video/framebuffer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+#include "definitions.h"
+
+
+class FrameBuffer {
+    public:
+        FrameBuffer(int, int);
+        virtual ~FrameBuffer();
+
+        Colour_t get_pixel(int, int);
+        void set_pixel(int, int, Colour_t);
+    
+    private:
+        std::vector<Colour_t> m_buffer;
+
+        int m_width;
+        int m_height;
+};
