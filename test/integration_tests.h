db06d3b707f91608658e81db80cc60f552d01cae
Aidan <aidanc@rogers.com>
2020-05-05 12:52:48 -0400

    Added some integration tests

---
+++ test/integration_tests.h
@@ -0,0 +1,75 @@
+#include "gtest/gtest.h"
+
+
+TEST(Integration, WriteIOFromMemoryMap) {
+    uint8_t bgp = 0xAB;
+    
+    MemoryMap memory_map;
+    CPU cpu(memory_map);
+    Video video(memory_map);
+
+    memory_map.write(BGP, bgp);
+    EXPECT_EQ(bgp, memory_map.read(BGP));
+    EXPECT_EQ(bgp, cpu.read_io_register(BGP));
+    EXPECT_EQ(bgp, video.read_io_register(BGP));
+}
+
+
+TEST(Integration, WriteIOFromCPU) {
+    uint8_t bgp = 0xAB;
+    
+    MemoryMap memory_map;
+    CPU cpu(memory_map);
+    Video video(memory_map);
+
+    cpu.write_io_register(BGP, bgp);
+    EXPECT_EQ(bgp, memory_map.read(BGP));
+    EXPECT_EQ(bgp, cpu.read_io_register(BGP));
+    EXPECT_EQ(bgp, video.read_io_register(BGP));
+}
+
+
+TEST(Integration, WriteIOFromVideo) {
+    uint8_t bgp = 0xAB;
+    
+    MemoryMap memory_map;
+    CPU cpu(memory_map);
+    Video video(memory_map);
+
+    video.write_io_register(BGP, bgp);
+    EXPECT_EQ(bgp, memory_map.read(BGP));
+    EXPECT_EQ(bgp, cpu.read_io_register(BGP));
+    EXPECT_EQ(bgp, video.read_io_register(BGP));
+}
+
+
+TEST(Integration, WriteMemoryFromMemoryMap) {
+    uint16_t address = 0xA000;
+    uint8_t data = 0xAB;
+
+    MemoryMap memory_map;
+    CPU cpu(memory_map);
+
+    cpu.write_register("HL", address);
+    EXPECT_EQ(address, cpu.read_register("HL"));
+
+    memory_map.write(address, data);
+    EXPECT_EQ(data, memory_map.read(address));
+    EXPECT_EQ(data, cpu.read_memory());
+}
+
+
+TEST(Integration, WriteMemoryFromCPU) {
+    uint16_t address = 0xA000;
+    uint8_t data = 0xAB;
+
+    MemoryMap memory_map;
+    CPU cpu(memory_map);
+
+    cpu.write_register("HL", address);
+    EXPECT_EQ(address, cpu.read_register("HL"));
+
+    cpu.write_memory(data);
+    EXPECT_EQ(data, memory_map.read(address));
+    EXPECT_EQ(data, cpu.read_memory());
+}
