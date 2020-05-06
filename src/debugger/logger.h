70a9c5d47dccf8be897ed014b4c4049d99f7338f
Aidan Clyens <aidanc@rogers.com>
2020-05-06 16:33:08 -0400

    Create Logger class

---
+++ src/debugger/logger.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <cstdarg>
+#include <string>
+
+
+typedef enum LogLevel {
+    LOG_INFO
+} LogLevel_t;
+
+
+class Logger {
+    public:
+        Logger() = default;
+
+        void log(LogLevel_t, bool, const char *, ...);
+
+        std::string str_format(const char *, va_list);
+};
+
+
+extern Logger logger;
+
+
+#define log_info(...) logger.log(LOG_INFO, true, ##__VA_ARGS__);
+#define log_info_no_new_line(...) logger.log(LOG_INFO, false, ##__VA_ARGS__);
