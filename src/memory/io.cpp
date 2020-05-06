65b713c71de213adcfd6881ee87147062ce25fcb
Aidan <aidanc@rogers.com>
2020-05-05 20:38:46 -0400

    Add debug statements for IO registers

--- src/memory/io.cpp
+++ src/memory/io.cpp
@@ -28,48 +28,120 @@ IO::~IO() {
 }
 
 uint8_t IO::read(IORegisters_t address) {
-    #ifdef DEBUG
-    std::cout << "IO read: " << static_cast<int>(address) << std::endl;
-    #endif
-
     switch (address) {
         case P1:
+            #ifdef DEBUG
+            std::cout << "IO read: P1" << std::endl;
+            #endif
+
             return m_P1;
         case DIV:
+            #ifdef DEBUG
+            std::cout << "IO read: DIV" << std::endl;
+            #endif
+
             return m_DIV;
         case TIMA:
+            #ifdef DEBUG
+            std::cout << "IO read: TIMA" << std::endl;
+            #endif
+
             return m_TIMA;
         case TMA:
+            #ifdef DEBUG
+            std::cout << "IO read: TMA" << std::endl;
+            #endif
+
             return m_TMA;
         case TAC:
+            #ifdef DEBUG
+            std::cout << "IO read: TAC" << std::endl;
+            #endif
+
             return m_TAC;
         case IF:
+            #ifdef DEBUG
+            std::cout << "IO read: IF" << std::endl;
+            #endif
+
             return m_IF;
         case LCDC:
+            #ifdef DEBUG
+            std::cout << "IO read: LCDC" << std::endl;
+            #endif
+
             return m_LCDC;
         case STAT:
+            #ifdef DEBUG
+            std::cout << "IO read: STAT" << std::endl;
+            #endif
+
             return m_STAT;
         case SCY:
+            #ifdef DEBUG
+            std::cout << "IO read: SCY" << std::endl;
+            #endif
+
             return m_SCY;
         case SCX:
+            #ifdef DEBUG
+            std::cout << "IO read: SCX" << std::endl;
+            #endif
+
             return m_SCX;
         case LY:
+            #ifdef DEBUG
+            std::cout << "IO read: LY" << std::endl;
+            #endif
+
             return m_LY;
         case LYC:
+            #ifdef DEBUG
+            std::cout << "IO read: LYC" << std::endl;
+            #endif
+
             return m_LYC;
         case DMA:
+            #ifdef DEBUG
+            std::cout << "IO read: DMA" << std::endl;
+            #endif
+
             return m_DMA;
         case BGP:
+            #ifdef DEBUG
+            std::cout << "IO read: BGP" << std::endl;
+            #endif
+
             return m_BGP;
         case OBP0:
+            #ifdef DEBUG
+            std::cout << "IO read: OBP0" << std::endl;
+            #endif
+
             return m_OBP0;
         case OBP1:
+            #ifdef DEBUG
+            std::cout << "IO read: OBP1" << std::endl;
+            #endif
+
             return m_OBP1;
         case WY:
+            #ifdef DEBUG
+            std::cout << "IO read: WY" << std::endl;
+            #endif
+
             return m_WY;
         case WX:
+            #ifdef DEBUG
+            std::cout << "IO read: WX" << std::endl;
+            #endif
+
             return m_WX;
         case IE:
+            #ifdef DEBUG
+            std::cout << "IO read: IE" << std::endl;
+            #endif
+
             return m_IE;
 
         default:
@@ -79,66 +151,138 @@ uint8_t IO::read(IORegisters_t address) {
 }
 
 uint16_t IO::write(IORegisters_t address, uint8_t data) {
-    #ifdef DEBUG
-    std::cout << "IO write: " << static_cast<int>(address) << std::endl;
-    #endif
-
     switch (address) {
         case P1:
+            #ifdef DEBUG
+            std::cout << "IO write: P1" << std::endl;
+            #endif
+
             m_P1 = data;
             break;
         case DIV:
+            #ifdef DEBUG
+            std::cout << "IO write: DIV" << std::endl;
+            #endif
+
             m_DIV = data;
             break;
         case TIMA:
+            #ifdef DEBUG
+            std::cout << "IO write: TIMA" << std::endl;
+            #endif
+
             m_TIMA = data;
             break;
         case TMA:
+            #ifdef DEBUG
+            std::cout << "IO write: TMA" << std::endl;
+            #endif
+
             m_TMA = data;
             break;
         case TAC:
+            #ifdef DEBUG
+            std::cout << "IO write: TAC" << std::endl;
+            #endif
+
             m_TAC = data;
             break;
         case IF:
+            #ifdef DEBUG
+            std::cout << "IO write: IF" << std::endl;
+            #endif
+
             m_IF = data;
             break;
         case LCDC:
+            #ifdef DEBUG
+            std::cout << "IO write: LCDC" << std::endl;
+            #endif
+
             m_LCDC = data;
             break;
         case STAT:
+            #ifdef DEBUG
+            std::cout << "IO write: STAT" << std::endl;
+            #endif
+
             m_STAT = data;
             break;
         case SCY:
+            #ifdef DEBUG
+            std::cout << "IO write: SCY" << std::endl;
+            #endif
+
             m_SCY = data;
             break;
         case SCX:
+            #ifdef DEBUG
+            std::cout << "IO write: SCX" << std::endl;
+            #endif
+
             m_SCX = data;
             break;
         case LY:
+            #ifdef DEBUG
+            std::cout << "IO write: LY" << std::endl;
+            #endif
+
             m_LY = data;
             break;
         case LYC:
+            #ifdef DEBUG
+            std::cout << "IO write: LYC" << std::endl;
+            #endif
+
             m_LYC = data;
             break;
         case DMA:
+            #ifdef DEBUG
+            std::cout << "IO write: DMA" << std::endl;
+            #endif
+
             m_DMA = data;
             break;
         case BGP:
+            #ifdef DEBUG
+            std::cout << "IO write: BGP" << std::endl;
+            #endif
+
             m_BGP = data;
             break;
         case OBP0:
+            #ifdef DEBUG
+            std::cout << "IO write: OBP0" << std::endl;
+            #endif
+
             m_OBP0 = data;
             break;
         case OBP1:
+            #ifdef DEBUG
+            std::cout << "IO write: OBP1" << std::endl;
+            #endif
+
             m_OBP1 = data;
             break;
         case WY:
+            #ifdef DEBUG
+            std::cout << "IO write: WY" << std::endl;
+            #endif
+
             m_WY = data;
             break;
         case WX:
+            #ifdef DEBUG
+            std::cout << "IO write: WX" << std::endl;
+            #endif
+
             m_WX = data;
             break;
         case IE:
+            #ifdef DEBUG
+            std::cout << "IO write: IE" << std::endl;
+            #endif
+
             m_IE = data;
             break;
 
@@ -153,9 +297,15 @@ uint16_t IO::write(IORegisters_t address, uint8_t data) {
 void IO::increment_counter(IORegisters_t reg) {
     switch (reg) {
         case LY:
+            #ifdef DEBUG
+            std::cout << "IO: Incremented LY" << std::endl;
+            #endif
             m_LY++;
             break;
         case DIV:
+            #ifdef DEBUG
+            std::cout << "IO: Incremented DIV" << std::endl;
+            #endif
             m_DIV++;
             break;
         default:
