c8dad64e29a68d0269bf1bf3b156088dfa3ee359
Aidan <aidanc@rogers.com>
2020-05-06 16:33:08 -0400

    Create Logger class

---
+++ src/debugger/logger.cpp
@@ -0,0 +1,25 @@
+#include "logger.h"
+
+
+Logger logger;
+
+
+void Logger::log(LogLevel_t log_level, bool newline, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    std::string msg = this->str_format(fmt, args);
+    va_end(args);
+
+    if (newline) {
+        fprintf(stdout, "%s\n", msg.c_str());
+    }
+    else {
+        fprintf(stdout, "%s", msg.c_str());
+    }
+}
+
+std::string Logger::str_format(const char *fmt, va_list args) {
+    char buf[8192];
+    vsnprintf(buf, 8192, fmt, args);
+    return std::string(buf);
+}
