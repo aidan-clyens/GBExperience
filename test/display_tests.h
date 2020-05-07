8f650b44364131c6b187a2288dbdcefa159588f8
Aidan <aidanc@rogers.com>
2020-05-07 18:03:09 -0400

    Add test to show set pixels in pixel buffer

--- test/display_tests.h
+++ test/display_tests.h
@@ -24,3 +24,20 @@ TEST(Display, SetPixel) {
         display.render(buffer);
     }
 }
+
+
+TEST(Display, SetPixelAllPixels) {
+    Display display;
+    display.init_display("TEST");
+
+    FrameBuffer buffer(LCD_WIDTH, LCD_HEIGHT);
+    for (int y = 0; y < LCD_HEIGHT; y++) {
+        for (int x = 0; x < LCD_WIDTH; x++) {
+            buffer.set_pixel(x, y, DARK_GRAY);
+        }
+    }
+
+    for (int i = 0; i < DISPLAY_TIME; i++) {
+        display.render(buffer);
+    }
+}
