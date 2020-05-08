288edee78c239cfc2b34c426776f95d662ce13fa
Aidan <aidanc@rogers.com>
2020-05-08 16:28:34 -0400

    Include video definitions in test cases

--- test/includes.h
+++ test/includes.h
@@ -7,6 +7,7 @@
 #include "memory/memory.h"
 #include "memory/memory_map.h"
 #include "memory/io.h"
+#include "video/definitions.h"
 #include "video/framebuffer.h"
 #include "video/video.h"
 #include "video/tile.h"
