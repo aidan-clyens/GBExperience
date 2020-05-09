9d26c794612c25d03e32a93bcb94e40cbe587250
Aidan <aidanc@rogers.com>
2020-05-08 21:05:53 -0400

    Set logging enabled or not for different types

--- src/debugger/logger.cpp
+++ src/debugger/logger.cpp
@@ -5,6 +5,24 @@ Logger logger;
 
 
 void Logger::log(LogType_t log_type, bool newline, const char *fmt, ...) {
+    switch (log_type) {
+        case LOG_CPU:
+            if (!m_cpu_logging_enabled) return;
+            break;
+        case LOG_INTERRUPTS:
+            if (!m_interrupt_logging_enabled) return;
+            break;
+        case LOG_IO:
+            if (!m_io_logging_enabled) return;
+            break;
+        case LOG_VIDEO:
+            if (!m_video_logging_enabled) return;
+            break;
+        case LOG_MEMORY:
+            if (!m_memory_logging_enabled) return;
+            break;
+    }
+
     va_list args;
     va_start(args, fmt);
     std::string msg = this->str_format(fmt, args);
@@ -22,4 +40,24 @@ std::string Logger::str_format(const char *fmt, va_list args) {
     char buf[8192];
     vsnprintf(buf, 8192, fmt, args);
     return std::string(buf);
+}
+
+void Logger::enable_logging(LogType_t log_type, bool enable) {
+    switch (log_type) {
+        case LOG_CPU:
+            m_cpu_logging_enabled = enable;
+            break;
+        case LOG_INTERRUPTS:
+            m_interrupt_logging_enabled = enable;
+            break;
+        case LOG_IO:
+            m_io_logging_enabled = enable;
+            break;
+        case LOG_VIDEO:
+            m_video_logging_enabled = enable;
+            break;
+        case LOG_MEMORY:
+            m_memory_logging_enabled = enable;
+            break;
+    }
 }
--- src/debugger/logger.h
+++ src/debugger/logger.h
@@ -27,7 +27,16 @@ class Logger {
 
         void log(LogType_t, bool, const char *, ...);
 
+        void enable_logging(LogType_t, bool);
+
         std::string str_format(const char *, va_list);
+    
+    private:
+        bool m_cpu_logging_enabled = false;
+        bool m_interrupt_logging_enabled = false;
+        bool m_io_logging_enabled = false;
+        bool m_video_logging_enabled = false;
+        bool m_memory_logging_enabled = false;
 };
 
 
@@ -44,3 +53,14 @@ extern Logger logger;
 #define log_video_no_new_line(...) logger.log(LOG_VIDEO, false, ##__VA_ARGS__);
 #define log_memory(...) logger.log(LOG_MEMORY, true, ##__VA_ARGS__);
 #define log_memory_no_new_line(...) logger.log(LOG_MEMORY, false, ##__VA_ARGS__);
+
+#define enable_cpu_logging() logger.enable_logging(LOG_CPU, true);
+#define disable_cpu_logging() logger.enable_logging(LOG_CPU, false);
+#define enable_interrupt_logging() logger.enable_logging(LOG_INTERRUPTS, true);
+#define disable_interrupt_logging() logger.enable_logging(LOG_INTERRUPTS, false);
+#define enable_io_logging() logger.enable_logging(LOG_IO, true);
+#define disable_io_logging() logger.enable_logging(LOG_IO, false);
+#define enable_video_logging() logger.enable_logging(LOG_VIDEO, true);
+#define disable_video_logging() logger.enable_logging(LOG_VIDEO, false);
+#define enable_memory_logging() logger.enable_logging(LOG_MEMORY, true);
+#define disable_memory_logging() logger.enable_logging(LOG_MEMORY, false);
