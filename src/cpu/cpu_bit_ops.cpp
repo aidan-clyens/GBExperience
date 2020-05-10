ead2865a42cd7742720b64038de004775cace4fb
Aidan <aidanc@rogers.com>
2020-05-10 17:48:55 -0400

    Fix CPU log statements

--- src/cpu/cpu_bit_ops.cpp
+++ src/cpu/cpu_bit_ops.cpp
@@ -13,7 +13,7 @@ void CPU::test_bit(int n, const std::string &reg) {
     }
 
     
-    log_cpu("BIT %X, %s", n, reg);
+    log_cpu("BIT %X, %s", n, reg.c_str());
     
 
     result = ((val >> n) & 0x01) == 0x00;
@@ -36,7 +36,7 @@ void CPU::set_bit(int n, const std::string &reg) {
     }
 
     
-    log_cpu("SET %X, %s", n, reg);
+    log_cpu("SET %X, %s", n, reg.c_str());
     
 
     result = val | shift;
@@ -62,7 +62,7 @@ void CPU::reset_bit(int n, const std::string &reg) {
     }
 
     
-    log_cpu("RES %X, %s", n, reg);
+    log_cpu("RES %X, %s", n, reg.c_str());
     
 
     result = val & ~shift;
