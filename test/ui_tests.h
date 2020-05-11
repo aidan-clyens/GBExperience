8c63e1cb75e7966c2e44e21d7aea865267d84754
Aidan <aidanc@rogers.com>
2020-05-11 09:52:28 -0400

    Input class does not depend on MemoryMap

--- test/ui_tests.h
+++ test/ui_tests.h
@@ -47,8 +47,7 @@ TEST(UI, SetPixelAllPixels) {
 
 
 TEST(UI, InitInput) {
-    MemoryMap mem_map;
-    Input input(mem_map);
+    Input input;
 
     EXPECT_FALSE(input.get_button_pressed(RIGHT));
     EXPECT_FALSE(input.get_button_pressed(LEFT));
@@ -62,8 +61,7 @@ TEST(UI, InitInput) {
 
 
 TEST(UI, SetButtonPressed) {
-    MemoryMap mem_map;
-    Input input(mem_map);
+    Input input;
 
     input.set_button_pressed(RIGHT, true);
     EXPECT_TRUE(input.get_button_pressed(RIGHT));
@@ -72,142 +70,11 @@ TEST(UI, SetButtonPressed) {
 
 
 TEST(UI, SetButtonReleased) {
-    MemoryMap mem_map;
-    Input input(mem_map);
+    Input input;
 
     input.set_button_pressed(RIGHT, true);
     EXPECT_TRUE(input.get_button_pressed(RIGHT));
 
     input.set_button_pressed(RIGHT, false);
     EXPECT_FALSE(input.get_button_pressed(RIGHT));
-}
-
-
-TEST(UI, GetDPADToggled) {
-    MemoryMap mem_map;
-    Input input(mem_map);
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-    EXPECT_FALSE(input.dpad_toggled());
-    EXPECT_FALSE(input.buttons_toggled());
-
-    // Set port P14 to low
-    uint8_t p1 = 0xFF & ~P14;  // 1110 1111
-    mem_map.write(P1, p1);
-    EXPECT_TRUE(input.dpad_toggled());
-    EXPECT_FALSE(input.buttons_toggled());
-}
-
-
-TEST(UI, GetButtonsToggled) {
-    MemoryMap mem_map;
-    Input input(mem_map);
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-    EXPECT_FALSE(input.dpad_toggled());
-    EXPECT_FALSE(input.buttons_toggled());
-
-    // Set port P15 to low
-    uint8_t p1 = 0xFF & ~P15;  // 1101 1111
-    mem_map.write(P1, p1);
-    EXPECT_FALSE(input.dpad_toggled());
-    EXPECT_TRUE(input.buttons_toggled());
-}
-
-
-TEST(UI, SelectDPAD) {
-    MemoryMap mem_map;
-    Input input(mem_map);
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-
-    // Enable DPAD
-    uint8_t p1 = 0xFF & ~P14; // 1110 1111
-    mem_map.write(P1, p1);
-    EXPECT_TRUE(input.dpad_toggled());
-
-    // Set Left button pressed
-    input.set_button_pressed(LEFT, true);
-    EXPECT_TRUE(input.get_button_pressed(LEFT));
-    // Also set A button pressed
-    input.set_button_pressed(A, true);
-    EXPECT_TRUE(input.get_button_pressed(A));
-
-    // Only P11 for the Left button should be set to low in the P1 register
-    uint8_t result_p1 = 0xED;   // 1110 1101
-    EXPECT_EQ(result_p1, mem_map.read(P1));
-}
-
-
-TEST(UI, SelectButtons) {
-    MemoryMap mem_map;
-    Input input(mem_map);
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-
-    // Enable Buttons
-    uint8_t p1 = 0xFF & ~P15; // 1101 1111
-    mem_map.write(P1, p1);
-    EXPECT_TRUE(input.buttons_toggled());
-
-    // Set Left button pressed
-    input.set_button_pressed(LEFT, true);
-    EXPECT_TRUE(input.get_button_pressed(LEFT));
-    // Also set B button pressed
-    input.set_button_pressed(A, true);
-    EXPECT_TRUE(input.get_button_pressed(A));
-
-    // Only P10 for the A button should be set to low in the P1 register
-    uint8_t result_p1 = 0xDE;   // 1101 1110
-    EXPECT_EQ(result_p1, mem_map.read(P1));
-}
-
-
-TEST(UI, PressWKey) {
-    MemoryMap mem_map;
-    UI ui(mem_map);
-    ui.init_display("TEST");
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-
-    // Enable DPAD
-    uint8_t p1 = 0xFF & ~P14; // 1110 1111
-    mem_map.write(P1, p1);
-
-    sf::Keyboard::Key key = sf::Keyboard::Key::W;
-    // Press 'W' key
-    ui.set_key_pressed(key, true);
-    EXPECT_EQ(0xEB, mem_map.read(P1));  // 1110 1011
-
-    // Release 'W' key
-    ui.set_key_pressed(key, false);
-    EXPECT_EQ(0xEF, mem_map.read(P1)); // 1110 1111
-}
-
-
-TEST(UI, PressEscapeKey) {
-    MemoryMap mem_map;
-    UI ui(mem_map);
-    ui.init_display("TEST");
-
-    // Ports of P1 register are active low
-    EXPECT_EQ(0xFF, mem_map.read(P1));
-
-    // Enable Button
-    uint8_t p1 = 0xFF & ~P15; // 1101 1111
-    mem_map.write(P1, p1);
-
-    sf::Keyboard::Key key = sf::Keyboard::Key::Escape;
-    // Press 'Escape' key
-    ui.set_key_pressed(key, true);
-    EXPECT_EQ(0xD7, mem_map.read(P1));  // 1101 0111
-
-    // Release 'Escape' key
-    ui.set_key_pressed(key, false);
-    EXPECT_EQ(0xDF, mem_map.read(P1)); // 1101 1111
 }
