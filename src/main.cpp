4e451d790e0c94c98b47cc7535e1127bda369768
Aidan <aidanc@rogers.com>
2020-05-13 20:20:28 -0400

    Add functions to trim input argument strings

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,24 @@
 #include "gameboy.h"
 
 
+const std::string WHITESPACE = " \n\r\t\f\v";
+
+
+std::string ltrim(const std::string &s) {
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+std::string rtrim(const std::string &s) {
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
+std::string trim(const std::string &s) {
+    return rtrim(ltrim(s));
+}
+
+
 int main(int argc, char** argv) {
     std::string rom_file = "roms/DrMario.gb";
     
