b782aee6f21f1f2648ef1e728947d0b4efeab1dd
Aidan Clyens <aidanc@rogers.com>
2020-05-14 21:05:40 -0400

    Step through instructions if debug flag is enabled

--- src/debugger/debugger.cpp
+++ src/debugger/debugger.cpp
@@ -11,6 +11,22 @@ Debugger::~Debugger() {
 
 }
 
-void Debugger::tick() {
+DebugAction_t Debugger::get_input() {
+    std::string buf = "";
+    log_debug("Debug: 's': step, 'c': continue, 'q': quit");
+    std::cin >> buf;
 
+    if (!buf.empty()) {
+        if (buf == "s") {
+            return STEP;
+        }
+        else if (buf == "c") {
+            return CONTINUE;
+        }
+        else if (buf == "q") {
+            return QUIT; 
+        }
+    }
+
+    return NONE;
 }
--- src/debugger/debugger.h
+++ src/debugger/debugger.h
@@ -4,12 +4,20 @@
 #include "../ui/ui.h"
 
 
+typedef enum DebugAction {
+    NONE,
+    STEP,
+    CONTINUE,
+    QUIT
+} DebugAction_t;
+
+
 class Debugger {
     public:
         Debugger(UI &);
         virtual ~Debugger();
 
-        void tick();
+        DebugAction_t get_input();
     
     private:
         UI m_ui;
