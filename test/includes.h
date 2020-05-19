5928965d35d59c3927235a62a40ab456018bc952
Aidan <aidanc@rogers.com>
2020-05-19 11:06:49 -0400

    Move constants to global include file

--- test/includes.h
+++ test/includes.h
@@ -19,3 +19,11 @@
 
 
 const int DISPLAY_TIME = 50;
+
+const int BUFFER_SIZE = 2 * 32 * 1024;
+const std::string TEST_ROM = "../../test/test_roms/cpu_instrs/cpu_instrs.gb";
+const std::string ROM_TITLE = "CPU_INSTRS";
+const int ROM_SIZE = 4;
+const cartridge_type_t CARTRIDGE_TYPE = ROM_MBC1;
+const bool GB_COLOR_SUPPORTED = true;
+const bool SGB_SUPPORTED = false;
