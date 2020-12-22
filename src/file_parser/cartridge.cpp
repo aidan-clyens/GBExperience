f5c60e2714372fb4a3325ef7bcee7f08bae26d7e
Aidan <aidanc@rogers.com>
2020-12-22 13:53:26 -0500

    Add MBC1 cartridge support

--- src/file_parser/cartridge.cpp
+++ src/file_parser/cartridge.cpp
@@ -21,6 +21,12 @@ m_num_rom_banks(num_rom_banks)
 
         m_rom_banks.push_back(mem);
     }
+    std::cout << "initialized " << m_rom_banks.size() << " ROM banks" << std::endl;
+
+    if (m_rom_banks.size() != m_num_rom_banks) {
+        std::cerr << "Cartridge initialization failed!" << std::endl;
+        throw new std::exception;
+    }
 }
 
 Cartridge::~Cartridge() {
@@ -71,4 +77,114 @@ uint16_t ROMOnly::write(uint16_t address, uint8_t data) {
     std::cerr << "ROMOnly: Cannot write to ROM" << std::endl;
 
     return 0;
+}
+
+
+MBC1::MBC1(std::vector<char> file_buffer, int num_rom_banks):
+Cartridge(file_buffer, num_rom_banks),
+m_ram_enabled(false),
+m_rom_bank_bits(0x0),
+m_rom_bank_number(1),
+m_mode(ROM_MODE)
+{
+    Cartridge::set_cartridge_type(ROM_MBC1);
+}
+
+MBC1::~MBC1() {
+    
+}
+
+uint8_t MBC1::read(uint16_t address) {
+    if (address > 2 * ROM_BANK_SIZE) {
+        std::cerr << "MBC1 read: Address out of range" << std::endl;
+        throw new std::exception;
+    }
+
+    // Read from ROM bank 00
+    if (address <= 0x3FFF) {
+        log_warn("MBC1 Read from Bank: 0, Address: %X", address);
+        return m_rom_banks[0]->read_memory(address);
+    }
+    // Read from ROM bank 01 -7F
+    else if (address >= 0x4000 & address <= 0x7FFF) {
+        address = address - 0x4000;
+        log_warn("MBC1 Read from Bank: %d, Address: %X", m_rom_bank_number, address);
+        if (m_rom_bank_number >= m_rom_banks.size()) {
+            std::cerr << "Error: ROM Bank number greater than expected size" << std::endl;
+            throw new std::exception;
+        }
+
+        return m_rom_banks[m_rom_bank_number]->read_memory(address);
+    }
+
+    return 0x0;
+}
+
+uint16_t MBC1::write(uint16_t address, uint8_t data) {
+    log_warn("Writing %X to MBC1 address: %X", data, address);
+
+    unsigned int index = (unsigned int)address / ROM_BANK_SIZE;
+    if (address > 2 * ROM_BANK_SIZE) {
+        std::cerr << "MBC1 write: Address out of range" << std::endl;
+        throw new std::exception;
+    }
+
+    // RAM Enable
+    if (address <= 0x1FFF) {
+        m_ram_enabled = ((data & 0xF) == 0x0A);
+        log_warn("MBC1 RAM Enable: %d", m_ram_enabled);
+    }
+    // ROM Bank Number
+    else if (address >= 0x2000 && address <= 0x3FFF) {
+        m_rom_bank_bits = (data & 0x1F);
+        m_rom_bank_number = (int)m_rom_bank_bits % m_num_rom_banks;
+        if (m_rom_bank_number == 0) {
+            m_rom_bank_number = 1;
+        }
+        log_warn("MBC1 ROM Bank Number (lower bits): %d", m_rom_bank_number);
+    }
+    // RAM Bank Number or ROM Bank Number Upper Bits
+    else if (address >= 0x4000 && address <= 0x5FFF) {
+        data &= 0x3;
+        switch (m_mode) {
+            case ROM_MODE:
+                m_rom_bank_bits = m_rom_bank_bits | (data << 5);
+                m_rom_bank_number = (int)m_rom_bank_bits;
+                if (m_rom_bank_number == 0) {
+                    m_rom_bank_number = 1;
+                }
+                log_warn("MBC1 ROM Bank Number (with upper bits): %d", m_rom_bank_number);
+                break;
+            case RAM_MODE:
+                log_warn("MBC1 RAM Bank Number: %X", data);
+                break;
+        }
+    }
+    // Banking Mode Select
+    else if (address >= 0x6000 && address <= 0x7FFF) {
+        if (data == 0x00) {
+            m_mode = ROM_MODE;
+        }
+        else if (data == 0x01) {
+            m_mode = RAM_MODE;
+        }
+        log_warn("MBC1 Banking Mode Select: %s", (m_mode == ROM_MODE) ? "ROM_MODE" : "RAM_MODE");
+    }
+    else {
+        // m_rom_buffer[index] = data;
+    }
+
+    return address;
+}
+
+bool MBC1::is_ram_enabled() const {
+    return m_ram_enabled;
+}
+
+int MBC1::get_rom_bank_number() const {
+    return m_rom_bank_number;
+}
+
+mode_select_t MBC1::get_mode() const {
+    return m_mode;
 }
