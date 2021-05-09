75be6650ba523e6173c46f334d59ac0be48c08b8
Aidan <aidanc@rogers.com>
2021-05-09 11:06:18 -0400

    Link Qt5 libraries and modify main file to run QApplication

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,55 @@
 #include <fstream>
 #include <iostream>
 
+#include <QtWidgets/QApplication>
+
 #include "debugger/logger.h"
 #include "utils/string_utils.h"
 #include "gameboy.h"
 
-
 int main(int argc, char** argv) {
-    std::string rom_file = "";
-    bool debugger_enabled = false;
-    bool headless = false;
+    QApplication app(argc, argv);
 
-    if (argc > 1) {
-        rom_file = argv[1];
-    }
-
-    if (argc > 2) {
-        for (int i = 2; i < argc; i++) {
-            std::string arg = trim(std::string(argv[i]));
-
-            if (arg == "--debug") {
-                debugger_enabled = true;
-            }
-
-            if (arg == "--headless") {
-                headless = true;
-            }
-
-            if (arg == "--warnings") {
-                enable_warn_logging();
-            }
-
-            if (arg == "--trace") {
-                enable_cpu_logging();
-            }
-        }
-    }
-
-    GameBoy gb(debugger_enabled, headless);
-    gb.load_rom(rom_file);
-
-    while (gb.is_display_open()) {
-        gb.tick();
-    }
-
-    return 0;
+    return app.exec();
 }
+
+// int main(int argc, char** argv) {
+//     std::string rom_file = "";
+//     bool debugger_enabled = false;
+//     bool headless = false;
+
+//     if (argc > 1) {
+//         rom_file = argv[1];
+//     }
+
+//     if (argc > 2) {
+//         for (int i = 2; i < argc; i++) {
+//             std::string arg = trim(std::string(argv[i]));
+
+//             if (arg == "--debug") {
+//                 debugger_enabled = true;
+//             }
+
+//             if (arg == "--headless") {
+//                 headless = true;
+//             }
+
+//             if (arg == "--warnings") {
+//                 enable_warn_logging();
+//             }
+
+//             if (arg == "--trace") {
+//                 enable_cpu_logging();
+//             }
+//         }
+//     }
+
+//     GameBoy gb(debugger_enabled, headless);
+//     gb.load_rom(rom_file);
+
+//     while (gb.is_display_open()) {
+//         gb.tick();
+//     }
+
+//     return 0;
+// }
